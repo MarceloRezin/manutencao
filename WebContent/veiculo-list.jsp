32766d4db99a2d9651a5a94fdedca524bbe89dcf
Marcelo Rezin <marcelo.rezin@gmail.com>
2019-12-08 21:47:17 -0200

    Listagem de veículos

---
+++ WebContent/veiculo-list.jsp
@@ -0,0 +1,50 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8" session="true" import="model.Veiculo"%>
+
+<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c" %>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Manutenção</title>
+<link rel="stylesheet" href="styles.css">
+</head>
+<body style="text-align: center">
+
+	<div class="topnav"> 
+		<a style="float: left; margin-right: 20px; font-weight: bold; font-size: 19px; border-right: 1px solid white">Gerenciamento de Manutenções</a>
+		<a href="/manutencao/">Home</a>
+		<a class="active" href="/manutencao/veiculo">Veículos</a>
+		<a href="/manutencao/manutencao">Itens</a>
+		<a href="/manutencao/manutencao">Manutenções</a>
+	</div>
+
+	<div>
+		<h2>${sessionScope.titulo}</h2>
+	</div>
+
+	<a href='/manutencao/veiculo/novo' class='button buttongreen'>Novo Veículo</a><br /><br />
+	
+	<input type="text" placeholder="Buscar" id="busca" value="${sessionScope.busca}" /><a onclick="this.href='/manutencao/veiculo?q='+document.getElementById('busca').value" href='' class='button buttonlightgreen'>Buscar</a>
+	<table id="listagem" align="center">
+		<tr>
+			<th>Id</th>
+			<th>Descrição</th>
+			<th>Placa</th>
+			<th>Tipo</th>
+			<th>Ano</th>
+			<th></th>
+		</tr>
+		<c:forEach var="v" items="${sessionScope.veiculos}">
+	   		<tr>
+				<td>${v.id}</td>
+				<td>${v.descricao}</td>
+				<td>${v.placa}</td>
+				<td>${v.tipo.descricao}</td>
+				<td>${v.ano}</td>
+				<td><a href="?id=${v.id}">Editar</a></td>
+			</tr>
+		</c:forEach>
+	</table>
+</body>
+</html>
--- src/controller/VeiculoServlet.java
+++ src/controller/VeiculoServlet.java
@@ -1,6 +1,7 @@
 package controller;
 
 import java.io.IOException;
+import java.sql.SQLException;
 import java.util.List;
 
 import javax.servlet.ServletException;
@@ -9,8 +10,10 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import exception.ManutencaoException;
 import model.Veiculo;
-import service.VeiculoService;
+import model.VeiculoDao;
+import model.VeiculoTipo;
 
 @WebServlet("/veiculo")
 public class VeiculoServlet extends HttpServlet {
@@ -20,24 +23,76 @@ public class VeiculoServlet extends HttpServlet {
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		
 		try {
-			
-			
-			List<Veiculo> v = VeiculoService.getInstance().list(null);
-			
-			for(Veiculo vei : v) {
-				response.getWriter().append(vei.getId().toString()).append("\t").append(vei.getDescricao()).append("\n");
+			if(request.getRequestURI().endsWith("/novo")) { //NEW
+				
+			}else {
+				
+				String id = request.getParameter("id");
+				boolean idInformado = id != null && id.length() > 0;
+				
+				if(request.getRequestURI().endsWith("/delete")){
+					if(idInformado) { //DELETE
+						VeiculoDao.getInstance().delete(Integer.parseInt(id));
+					}else {
+						throw new ManutencaoException("Não foi encontrado o veículo informado.");
+					}
+				}else {
+					if(idInformado) { //GET
+						Veiculo veiculo = VeiculoDao.getInstance().get(Integer.parseInt(id));
+					}else { //LIST
+						String query = request.getParameter("q");
+						
+						List<Veiculo> veiculos = VeiculoDao.getInstance().list(query);
+						request.getSession().setAttribute("titulo", "Listagem de Veículos");
+						request.getSession().setAttribute("busca", query);
+						request.getSession().setAttribute("veiculos", veiculos);
+						
+						request.getRequestDispatcher("/veiculo-list.jsp").forward(request, response);
+					}
+				}
 			}
-		
-		} catch (Exception e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+				
+		}catch(ClassNotFoundException | SQLException e1) {
+			//TODO: Erro banco
+		}catch(ManutencaoException e2) {
+			//TODO: Erro normal
+		}catch(Exception e3) {
+			//TODO: Erro não esperado
 		}
 	}
 
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		doGet(request, response);
+		try {
+			
+			Veiculo veiculoSave;
+			
+			if(request.getRequestURI().endsWith("/new")) { //NEW
+				veiculoSave = new Veiculo(null, Integer.parseInt(request.getParameter("ano")), request.getParameter("placa"), VeiculoTipo.valueOf(request.getParameter("tipo")), request.getParameter("descricao"));
+			}else {
+				String id = request.getParameter("id");
+				
+				if(id == null || id.length() < 1) {
+					throw new ManutencaoException("Não foi encontrado o veículo informado.");
+				}
+				
+				veiculoSave = VeiculoDao.getInstance().get(Integer.parseInt(id));
+
+				veiculoSave.setAno(Integer.parseInt(request.getParameter("ano")));
+				veiculoSave.setPlaca(request.getParameter("placa"));
+				veiculoSave.setTipo(VeiculoTipo.valueOf(request.getParameter("tipo")));
+				veiculoSave.setDescricao(request.getParameter("descricao"));
+			}
+			
+			VeiculoDao.getInstance().save(veiculoSave);
+				
+		}catch(ClassNotFoundException | SQLException e1) {
+			//TODO: Erro banco
+		}catch(ManutencaoException e2) {
+			//TODO: Erro normal
+		}catch(Exception e3) {
+			//TODO: Erro não esperado
+		}
 	}
 
 }
